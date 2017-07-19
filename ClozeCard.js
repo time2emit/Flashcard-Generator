6f00bb63711cb0d47a6dbf19d4a38f583a580e38
Alexis Frank <time2emit@gmail.com>
2017-07-18 23:05:44 -0700

    cloze update

--- ClozeCard.js
+++ ClozeCard.js
@@ -6,17 +6,62 @@
 // The constructed object should have a fullText property that contains only the full text.
 // The constructor should throw or log an error when the cloze deletion does not appear in the input text.
 // Use prototypes to attach these methods, wherever possible.
+// var fs = require('fs');
+// var inquirer = require('inquirer');
+// module.exports = ClozeCard;
 
-var inquirer = require('inquirer');
-module.exports = ClozeCard;
 
-var ClozeCard = function(text, cloze) {
+
+function ClozeCard (text, cloze) {
 	this.cloze = cloze;
-	this.partial = partial;
-	this.fullText = fullText;
-	this.error = function() {
-		if (!cloze in text) {
-			return console.log("Cloze deletion must be in the question text");
-		}
+	this.text = text;
+	if (this.checkCloze() === false) {
+		throw this.cloze + " must be in the text. \n Text is:" + this.text;
+	}
+	this.partial = this.makePartial();
+}
+
+ClozeCard.prototype.checkCloze = function() {
+	if (this.text.includes(this.cloze)) {
+		return true;
+		// console.log(this.cloze + " must be in the text. \n Text is:" + this.text);
+	} else {
+		return false;
 	}
 };
+
+ClozeCard.prototype.makePartial = function() {
+	return this.text.replace(this.cloze, "...");
+
+
+}
+
+
+
+try {
+	var georgeWashington = new ClozeCard("George Washington was the first President of the USA","George Washington");
+
+	// var evilGeorge = new ClozeCard("George Washington was the first President of the USA","Evil George");
+
+	console.log(georgeWashington);
+	console.log(georgeWashington.checkCloze());
+	console.log(georgeWashington.makePartial());
+	console.log(georgeWashington.partial);
+
+
+	// console.log(evilGeorge);
+	// console.log(evilGeorge.checkCloze());
+
+} catch (e) {
+	console.log(e);
+}
+
+
+
+
+
+// }
+// Person.prototype.name = function() {
+//     return this.firstName + " " + this.lastName;
+// };
+
